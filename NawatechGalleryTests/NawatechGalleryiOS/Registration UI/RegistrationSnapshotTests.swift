64fae11d02757608f0bb66411f8c0a3c76a53f5b
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-19 21:22:29 +0700

    Registration UI page with no all requirement fields is filled snapshot test

--- NawatechGalleryTests/NawatechGalleryiOS/Registration UI/RegistrationSnapshotTests.swift
+++ NawatechGalleryTests/NawatechGalleryiOS/Registration UI/RegistrationSnapshotTests.swift
@@ -16,6 +16,13 @@ final class RegistrationSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(configuration: .iPhone13(style: .light)), named: "REGISTRATION_INITIAL_SETUP_light")
     }
     
+    func test_registrationWithNotAllOfFieldsIsFilled() {
+        let sut = makeSUT()
+        sut.fillFields(fullname: "Test fullname", username: "Test username", password: nil)
+        
+        assert(snapshot: sut.snapshot(configuration: .iPhone13(style: .light)), named: "REGISTRATION_NOT_ALL_FIELDS_FILLED_light")
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT() -> RegistrationViewController {
@@ -27,3 +34,15 @@ final class RegistrationSnapshotTests: XCTestCase {
         return controller
     }
 }
+
+private extension RegistrationViewController {
+    func fillFields(fullname: String?, username: String?, password: String?) {
+        fullnameField.text = fullname
+        usernameField.text = username
+        passwordField.text = password
+        textFieldDidEndEditing(fullnameField)
+        textFieldDidEndEditing(usernameField)
+        textFieldDidEndEditing(passwordField)
+    }
+}
+
