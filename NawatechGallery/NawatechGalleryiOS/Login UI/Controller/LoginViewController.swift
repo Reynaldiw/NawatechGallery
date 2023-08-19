ee708e83de1c7c6f2b2a988b6c4a778a01a34041
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-19 20:06:21 +0700

    Remove unused code.

--- NawatechGallery/NawatechGalleryiOS/Login UI/Controller/LoginViewController.swift
+++ NawatechGallery/NawatechGalleryiOS/Login UI/Controller/LoginViewController.swift
@@ -15,12 +15,6 @@ public final class LoginViewController: UIViewController {
     @IBOutlet private(set) public var signUpButton: UIButton!
     @IBOutlet private(set) public var skipLoginButton: UIButton!
     
-    private var isLoginButtonEnable: Bool = false {
-        didSet {
-            updateLoginButtonUIState(isEnable: isLoginButtonEnable)
-        }
-    }
-
     public override func viewDidLoad() {
         super.viewDidLoad()
         
