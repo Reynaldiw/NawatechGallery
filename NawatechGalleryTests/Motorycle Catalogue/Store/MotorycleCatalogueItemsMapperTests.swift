83e0dcb84dbd2f897d391752449f9c42c152bbcc
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-20 12:57:29 +0700

    `map` command, delivers error on invalid data.

--- NawatechGalleryTests/Motorycle Catalogue/Store/MotorycleCatalogueItemsMapperTests.swift
+++ NawatechGalleryTests/Motorycle Catalogue/Store/MotorycleCatalogueItemsMapperTests.swift
@@ -9,17 +9,45 @@ import XCTest
 import NawatechGallery
 
 final class MotorycleCatalogueItemsMapper {
-    private init() {}
+    private struct StoredMotorycleCatalogueItem: Decodable {
+        
+        private enum CodingKeys: String, CodingKey {
+            case id, name, detail, price
+            case imageURL = "image_url"
+        }
+        
+        let id: UUID
+        let imageURL: URL
+        let name: String
+        let detail: String
+        let price: Int
+    }
     
-    static func map(_ data: Data) -> [MotorcycleCatalogueItem] {
+    enum Error: Swift.Error {
+        case invalidData
+    }
+        
+    static func map(_ data: Data) throws -> [MotorcycleCatalogueItem] {
+        guard let _ = try? JSONDecoder().decode([StoredMotorycleCatalogueItem].self, from: data) else {
+            throw Error.invalidData
+        }
+        
         return []
     }
 }
 
 final class MotorycleCatalogueItemsMapperTests: XCTestCase {
     
-    func test_map_deliversNoItemsOnEmptyValues() {
-        let result = MotorycleCatalogueItemsMapper.map(makeItemValues([]))
+    func test_map_deliversErrorOnInvalidData() throws {
+        let invalidData = Data("Invalid values".utf8)
+        
+        XCTAssertThrowsError(
+            try MotorycleCatalogueItemsMapper.map(invalidData)
+        )
+    }
+    
+    func test_map_deliversNoItemsOnEmptyValues() throws {
+        let result = try MotorycleCatalogueItemsMapper.map(makeItemValues([]))
         
         XCTAssertEqual(result, [])
     }
