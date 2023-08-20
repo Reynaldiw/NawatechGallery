0a1d01686046f09d86e72488687c9460e6810cf1
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-20 12:31:33 +0700

    Create `MotorycleContentStore` as boundaries to retrieve motorcycle content in store.

---
+++ NawatechGallery/Motorcycle Content/Store/MotorycleContentStore.swift
@@ -0,0 +1,12 @@
+//
+//  MotorycleContentStore.swift
+//  NawatechGallery
+//
+//  Created by Reynaldi on 20/08/23.
+//
+
+import Foundation
+
+public protocol MotorycleContentStore {
+    func retrieve() throws -> [[String: Any]]
+}
