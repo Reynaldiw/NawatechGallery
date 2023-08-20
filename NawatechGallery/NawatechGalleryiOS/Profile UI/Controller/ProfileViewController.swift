75deba11ef661d5b5f9ad18437f02e09c1114165
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-20 19:05:22 +0700

    Connect `ProfileViewController` with `Profile.storyboard` and configure the initial setup UI.

---
+++ NawatechGallery/NawatechGalleryiOS/Profile UI/Controller/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  NawatechGallery
+//
+//  Created by Reynaldi on 20/08/23.
+//
+
+import UIKit
+
+public final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private(set) public var profileImageView: UIImageView!
+    @IBOutlet private(set) public var editProfileImageButton: UIButton!
+    @IBOutlet private(set) public var nameLabel: UILabel!
+    
+    public var retrieveProfile: (() -> Void)?
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureInitialUI()
+        
+        retrieveProfile?()
+    }
+    
+    private func configureInitialUI() {
+        profileImageView.isHidden = true
+        editProfileImageButton.isHidden = true
+        nameLabel.isHidden = true
+    }
+    
+    @IBAction private func didTapEditProfileImage(_ sender: Any) {}
+}
