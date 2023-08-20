fbfaf51a96605dbe20a37592fd9f9d185613bfb0
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-20 12:28:23 +0700

    Create `MotorcycleItem` as domain model for Content Motorcycle.

---
+++ NawatechGallery/Motorcycle Content/Feature/MotorcycleItem.swift
@@ -0,0 +1,16 @@
+//
+//  MotorcycleItem.swift
+//  NawatechGallery
+//
+//  Created by Reynaldi on 20/08/23.
+//
+
+import Foundation
+
+public struct MotorcycleItem {
+    public let id: UUID
+    public let imageURL: URL
+    public let name: String
+    public let detail: String
+    public let price: Int
+}
