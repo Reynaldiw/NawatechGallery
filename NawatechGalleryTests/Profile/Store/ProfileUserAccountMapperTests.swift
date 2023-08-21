51cc1221337a4fd64d9a8546308be99b61f93707
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-21 21:13:33 +0700

    map command, delivers error on empty values.

--- NawatechGalleryTests/Profile/Store/ProfileUserAccountMapperTests.swift
+++ NawatechGalleryTests/Profile/Store/ProfileUserAccountMapperTests.swift
@@ -16,4 +16,38 @@ final class ProfileUserAccountMapperTests: XCTestCase {
             try ProfileUserAccountMapper.map(invalidData)
         )
     }
+    
+    func test_map_deliversErrorOnEmptyValues() throws {
+        XCTAssertThrowsError(try ProfileUserAccountMapper.map(makeUserValues([])))
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeUser(
+        id: UUID,
+        profileImageURL: URL?,
+        fullname: String,
+        username: String,
+        password: String,
+        createdAt date: Date
+    ) -> (model: ProfileUserAccount, value: [String: Any]) {
+        let model = ProfileUserAccount(id: id, profileImageURL: profileImageURL, fullname: fullname)
+        var value: [String: Any] = [
+            "id": id.uuidString,
+            "fullname": fullname,
+            "username": username,
+            "password": password,
+            "created_at": date,
+        ]
+        
+        if let profileImageURL = profileImageURL {
+            value["profile_image_url"] = profileImageURL.absoluteString
+        }
+        
+        return (model, value)
+    }
+    
+    private func makeUserValues(_ users: [[String: Any]]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: users)
+    }
 }
