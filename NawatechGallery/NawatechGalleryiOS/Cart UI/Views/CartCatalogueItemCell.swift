72bb0a624f3ac85d04e7068b868991469c67fdbe
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-22 20:11:01 +0700

    Create `CartCatalogueItemCell` as UITableViewCell and connect it with the UI in storyboard

---
+++ NawatechGallery/NawatechGalleryiOS/Cart UI/Views/CartCatalogueItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  CartCatalogueItemCell.swift
+//  NawatechGallery
+//
+//  Created by Reynaldi on 22/08/23.
+//
+
+import UIKit
+
+public final class CartCatalogueItemCell: UITableViewCell {
+
+    @IBOutlet private(set) public var itemImageContainerView: UIView!
+    @IBOutlet private(set) public var itemImageView: UIImageView!
+    @IBOutlet private(set) public var titleLabel: UILabel!
+    @IBOutlet private(set) public var itemPriceLabel: UILabel!
+    @IBOutlet private(set) public var quantityLabel: UILabel!
+    @IBOutlet private(set) public var itemStepper: UIStepper!
+    @IBOutlet private(set) public var totalItemPrice: UILabel!
+    
+    var onReuse: (() -> Void)?
+    
+    public override func prepareForReuse() {
+        onReuse?()
+    }
+}
