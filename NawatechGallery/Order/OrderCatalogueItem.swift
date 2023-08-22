e9b09e83579198fed868d7d1533fc99b3b5501b6
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-08-22 16:24:30 +0700

    Create `Order` feature domain model and mapper to map `Data` to domain model.

---
+++ NawatechGallery/Order/OrderCatalogueItem.swift
@@ -0,0 +1,14 @@
+//
+//  OrderCatalogueItem.swift
+//  NawatechGallery
+//
+//  Created by Reynaldi on 22/08/23.
+//
+
+import Foundation
+
+public struct OrderCatalogueItem {
+    public let id: String
+    public let catalogueID: String
+    public let quantity: Int
+}
---
+++ NawatechGallery/Order/OrderCatalogueItemsMapper.swift
@@ -0,0 +1,38 @@
+//
+//  OrderCatalogueItemsMapper.swift
+//  NawatechGallery
+//
+//  Created by Reynaldi on 22/08/23.
+//
+
+import Foundation
+
+public final class OrderCatalogueItemsMapper {
+    
+    private struct StoredOrderCatalogueItem: Decodable {
+        enum CodingKeys: String, CodingKey {
+            case id = "order_id"
+            case catalogueID = "catalogue_item_id"
+            case quantity
+        }
+        
+        let id: String
+        let catalogueID: String
+        let quantity: Int
+        
+        var model: OrderCatalogueItem {
+            OrderCatalogueItem(id: id, catalogueID: catalogueID, quantity: quantity)
+        }
+    }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data) throws -> [OrderCatalogueItem] {
+        guard let storedOrders = try? JSONDecoder().decode([StoredOrderCatalogueItem].self, from: data)
+        else { throw Error.invalidData }
+        
+        return storedOrders.map { $0.model }
+    }
+}
